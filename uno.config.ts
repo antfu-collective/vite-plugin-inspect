486148de5e57835ab565f3dc87cb986da4096f6e
sun0day <ivan.xu@applovin.com>
2023-04-15 16:32:47 +0800

    fix: unexpected dark bg in light mode (#68)

--- uno.config.ts
+++ uno.config.ts
@@ -11,7 +11,7 @@ import {
 export default defineConfig({
   shortcuts: {
     'border-main': 'border-gray-400 border-opacity-30',
-    'bg-main': 'bg-white bg-[#121212]',
+    'bg-main': 'bg-white dark:bg-[#121212]',
     'bg-active': 'bg-gray-400/10',
     'icon-btn': 'inline-block cursor-pointer select-none !outline-none '
       + 'opacity-75 transition duration-200 ease-in-out '
