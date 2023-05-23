1a74fe1458504b3acdc73999f0a583e6c8e16ec8
Anthony Fu <anthonyfu117@hotmail.com>
2023-05-23 14:41:24 +0200

    feat: show production marker in title

--- src/client/App.vue
+++ src/client/App.vue
@@ -1,3 +1,12 @@
+<script setup lang="ts">
+import { isStaticMode } from './logic'
+
+onMounted(() => {
+  if (isStaticMode)
+    document.title = 'Vite Inspect (Production)'
+})
+</script>
+
 <template>
   <main grid="~ rows-[min-content_1fr]" size="h-screen w-screen" text="gray-700 dark:gray-200">
     <Suspense>
