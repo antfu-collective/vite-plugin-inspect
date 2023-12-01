efae940830a7a2abf408f47a9711f8100f96b415
userquin <userquin@gmail.com>
2023-12-01 23:00:07 +0100

    fix: default CJS exports

--- scripts/postbuild.ts
+++ scripts/postbuild.ts
@@ -1,23 +1,16 @@
-import { readFileSync, readdirSync, rmSync, writeFileSync } from 'node:fs'
+import { readFileSync, writeFileSync } from 'node:fs'
 import { resolve } from 'node:path'
 
 function patchCjs(cjsModulePath: string, name: string) {
-  const cjsModule = readFileSync(cjsModulePath, 'utf-8')
+  const cjsFile = `${cjsModulePath}.cjs`
+  const cjsModule = readFileSync(cjsFile, 'utf-8')
   writeFileSync(
-    cjsModulePath,
-    cjsModule.replace(`module.exports = ${name};`, `exports.default = ${name};`),
+    cjsFile,
+    cjsModule.replace(`module.exports = ${name};`, `exports.default = ${name};\nexports.__esModule = true;`),
+    // cjsModule.replace(`module.exports = ${cjsName};`, `module.exports = ${cjsName};\nexports.default = ${cjsName};`),
     { encoding: 'utf-8' },
   )
 }
 
-rmSync(resolve('./dist/index.d.mts'))
-rmSync(resolve('./dist/nuxt.d.mts'))
-
-readdirSync('./dist/shared')
-  .filter(file => file.endsWith('.d.mts'))
-  .forEach((file) => {
-    rmSync(resolve('./dist/shared', file))
-  })
-
-patchCjs(resolve('./dist/index.cjs'), 'PluginInspect')
-patchCjs(resolve('./dist/nuxt.cjs'), 'nuxt')
+patchCjs(resolve('./dist/index'), 'PluginInspect')
+patchCjs(resolve('./dist/nuxt'), 'nuxt')
