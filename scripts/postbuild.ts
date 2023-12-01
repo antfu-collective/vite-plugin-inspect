3bfe74bd37d5f2ab1c85763cc1b0936baffc9279
userquin <userquin@gmail.com>
2023-12-01 23:06:50 +0100

    chore: revert cjs module extension

--- scripts/postbuild.ts
+++ scripts/postbuild.ts
@@ -2,15 +2,14 @@ import { readFileSync, writeFileSync } from 'node:fs'
 import { resolve } from 'node:path'
 
 function patchCjs(cjsModulePath: string, name: string) {
-  const cjsFile = `${cjsModulePath}.cjs`
-  const cjsModule = readFileSync(cjsFile, 'utf-8')
+  const cjsModule = readFileSync(cjsModulePath, 'utf-8')
   writeFileSync(
-    cjsFile,
+    cjsModulePath,
     cjsModule.replace(`module.exports = ${name};`, `exports.default = ${name};\nexports.__esModule = true;`),
     // cjsModule.replace(`module.exports = ${cjsName};`, `module.exports = ${cjsName};\nexports.default = ${cjsName};`),
     { encoding: 'utf-8' },
   )
 }
 
-patchCjs(resolve('./dist/index'), 'PluginInspect')
-patchCjs(resolve('./dist/nuxt'), 'nuxt')
+patchCjs(resolve('./dist/index.cjs'), 'PluginInspect')
+patchCjs(resolve('./dist/nuxt.cjs'), 'nuxt')
